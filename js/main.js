09268c406c1cdcb12413621bc700d1ba4dad31a5
Max Armino <maxxarmino@protonmail.com>
2018-08-27 20:39:08 +0200

    vecka + jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+Date.prototype.getWeek = function() {
+    var onejan = new Date(this.getFullYear(),0,1);
+    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+}
+
 function startTime() {
     var today = new Date();
+    var weekNumber = today.getWeek();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
@@ -7,6 +13,7 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
     h + ":" + m + ":" + s;
+    document.getElementById("vecka").innerHTML = "Vecka: " + weekNumber;
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
